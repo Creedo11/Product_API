7074bbea03bbcd76a925041fb92fc2e624b1fc28
Creedo11 <keegkree@gmail.com>
2022-07-25 22:06:37 -0400

    Modified Product List Function in Prodcut Views File

--- product/views.py
+++ product/views.py
@@ -1,9 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-
+from .serializers import ProductSerializer
+from .models import Product
+from product import serializers
 
 @api_view(['GET'])
 def product_list(request):
+    product = Product.objects.all()
+    serializer = ProductSerializer(product, many=True)
 
-    return Response("ok")
+    return Response(serializer.data)
+    
